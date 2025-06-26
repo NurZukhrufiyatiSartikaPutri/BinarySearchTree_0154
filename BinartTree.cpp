2950a0146d5d2e3a522367ffa2f750ac882c9c64
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-26 16:01:12 +0700

    Allocate memory for the new node

--- BinartTree.cpp
+++ BinartTree.cpp
@@ -40,4 +40,14 @@ public:
         }
     }
 
+    void insert()
+    {
+        int x;
+        cout << "Masukkan nilai: ";
+        cin >> x;
+
+        // step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+    }
 };
